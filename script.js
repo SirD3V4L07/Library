b56d03e258682b91780c2e0882d6af371bd76310
SirD3v4l07 <ascendantspiral@gmail.com>
2024-04-25 21:28:34 +0100

    Styling

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function displayBooks() {
     for (let i = 0; i < 20; i++) {
         document.getElementById("tiles").innerHTML += `
         <div class="book-tile">
-        <p>Title</p>
-        <p>Author</p>
-        <p>Pages</p>
+        <p>A Song of Ice and Fire</p>
+        <p>George R R Martin</p>
+        <p>600 Pages</p>
         <div class="read-status"><p>Read</p><img src="./media/notread.png"></div>
         </div>
         `;
