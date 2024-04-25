bd87d13d22a3e34f5ff6d5028d31d5918df96951
SirD3v4l07 <ascendantspiral@gmail.com>
2024-04-25 17:11:12 +0100

    Add additional tiles

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ let book2 = new Book("Sapiens", "Yuval", 512, false);
 bookList.push(book1);
 bookList.push(book2);
 
-function displayBooks(){
-    
+function displayBooks() {
+    document.getElementById("tiles").innerHTML += `
+    <div class="book-tile">
+    <p>Title:</p>
+    <p>Author:</p>
+    <p>Pages:</p>
+    <p>Read:</p>
+    </div>
+    `;
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById("tiles").innerHTML += `
+        <div class="book-tile">
+        <p>Title:</p>
+        <p>Author:</p>
+        <p>Pages:</p>
+        <p>Read:</p>
+        </div>
+        `;
+    }
 }
+
+displayBooks();
