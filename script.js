5a53ed4f8bb1fc89ad0bb96de94b7440ea332d9e
SirD3v4l07 <ascendantspiral@gmail.com>
2024-04-25 14:43:22 +0100

    Populate array with examples

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-bookList.push = new Book("Book Title", "Author Name", 233, false);
+let book1 = new Book("Book Title", "Author Name", 233, false);
+let book2 = new Book("Sapiens", "Yuval", 512, false);
+
+bookList.push(book1);
+bookList.push(book2);
+
+function displayBooks(){
+    
+}
