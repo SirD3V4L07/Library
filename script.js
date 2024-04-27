e61e1770db2b8613f343fa444b4757983d006df5
SirD3v4l07 <ascendantspiral@gmail.com>
2024-04-27 14:49:41 +0100

    Add new book to array and update screen

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+/*
 let book1 = new Book("Mastery", "Robert Greene", 600, false);
 let book2 = new Book("Sapiens", "Yuval Noah Harari", 512, false);
 
 bookList.push(book1);
 bookList.push(book2);
+*/
 
 function displayBooks() {
-    document.getElementById("tiles").innerHTML += `
-    <div id="book-form" class="book-tile">
-    <p>New Book</p>
-    <form method="post">
-    <input type="text" name="title" id="title-input" placeholder="Title" />
-    <input type="text" name="author" id="author-input" placeholder="Author" />
-    <input type="number" name="pages" id="pages-input" placeholder="Pages" />
-    <button type="submit">Submit</button>
-    </form>
-    </div>
-    `;
+    
+    let newBook = new Book(
+        document.getElementById("title-input").value,
+        document.getElementById("author-input").value,
+        document.getElementById("pages-input").value,
+        false
+        );
+    bookList.push(newBook);
 
+    resetTiles();
     for (let i = 0; i < bookList.length; i++) {
         document.getElementById("tiles").innerHTML += `
         <div class="book-tile">
@@ -38,4 +38,18 @@ function displayBooks() {
     }
 }
 
-displayBooks();
+function resetTiles() {
+    document.getElementById("tiles").innerHTML = `
+    <div id="book-form" class="book-tile">
+    <p>New Book</p>
+    <form method="post">
+    <input type="text" name="title" id="title-input" placeholder="Title" />
+    <input type="text" name="author" id="author-input" placeholder="Author" />
+    <input type="number" name="pages" id="pages-input" placeholder="Pages" />
+    <button type="submit" onclick="displayBooks()">Submit</button>
+    </form>
+    </div>
+    `;
+}
+
+resetTiles();
